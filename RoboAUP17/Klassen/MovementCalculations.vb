98e16d674a0470542eca9269eed9ce62859fbb7f
Philipp Bauer <philipp.bauer92@gmail.com>
2019-04-24 17:39:59 +0200

    Synchronisierung der Achsen, neue Idee

--- RoboAUP17/Klassen/MovementCalculations.vb
+++ RoboAUP17/Klassen/MovementCalculations.vb
@@ -58,58 +58,63 @@
     End Structure
 
     Private Structure profile_t
+        Friend v_min As Double()
         Friend a As Double()
         Friend v As Double()
     End Structure
 
-    Private Sub calcMaxTimes(ByRef _t As times_t, ByRef _v_max As Double(), ByRef _a_max As Double())
+    Private Sub calcMaxTimes(ByRef times As times_t, ByRef v_max As Double(), ByRef a_max As Double())
         Dim t As times_t
         Dim t_ges, t_max As Double
 
         'Benötigte Zeit jeder Achse berechnen
         For i = 0 To 5
             'Maximalwert ermitteln
-            t_ges = calcMoveTime(t, s(i), _v_max(i), _a_max(i))
+            t_ges = calcMoveTime(t, s(i), v_min(i), v_max(i), a_max(i))
             If t_ges >= t_max Then
                 t_max = t_ges
-                _t.t_const = t.t_const
-                _t.t_ac = t.t_ac
-                _t.v_max_reached = If(t.t_const = 0, False, True)
+                times.t_const = t.t_const
+                times.t_ac = t.t_ac
+                times.v_max_reached = If(t.t_const = 0, False, True)
             End If
         Next
     End Sub
-    Private Function calcProfile(ByRef _p As profile_t, ByRef _t As times_t) As Boolean
+    Private Function calcProfile(ByRef prof As profile_t, ByRef times As times_t) As Boolean
         Dim limit As Boolean = False
         For i = 0 To 5
             'Beschleunigung ermitteln
-            _p.a(i) = Math.Round((s(i) / (_t.t_ac * (_t.t_ac + _t.t_const))) * 100.0) / 100.0
-            'Geschwindigkeit ermitteln 
-            _p.v(i) = Math.Round((_p.a(i) * _t.t_ac) * 100.0) / 100.0
+            prof.a(i) = Math.Round((s(i) / (times.t_ac * (times.t_ac + times.t_const))) * 100.0) / 100.0
+            'Geschwindigkeit ermitteln
+            prof.v(i) = Math.Round((prof.a(i) * times.t_ac) * 100.0) / 100.0
             'Begrenzen
-            If _p.a(i) > a_max(i) Then
-                _p.a(i) = a_max(i)
+            If prof.a(i) > a_max(i) Then
+                prof.a(i) = a_max(i)
                 limit = True
             End If
-            If _p.v(i) > v_max(i) Then
-                _p.v(i) = v_max(i)
+            If prof.v(i) > v_max(i) Then
+                prof.v(i) = v_max(i)
                 limit = True
             End If
         Next
         Return Not limit
     End Function
-    Private Function calcMoveTime(ByRef _t As times_t, _s As Double, _v_max As Double, _a_max As Double) As Double
-        Dim s_acc_max As Double = _v_max ^ 2 / _a_max / 2 'benötigte Strecke bis v_max bei maximaler Beschleunigung
+    Private Function calcMoveTime(ByRef t As times_t, s As Double, v_min As Double, v_max As Double, a_max As Double) As Double
+        'benötigte Zeit bis v_max mit maximaler Beschleunigung
+        Dim t_ac As Double = (v_max - v_min) / a_max
+        'dafür benötigte Strecke
+        Dim s_acc_max As Double = ((a_max * t_ac ^ 2) / 2) + v_min * t_ac
+
         'Zeit für Beschleunigung und für Fahrt mit konstanter Geschwindigkeit
-        If _s - 2 * s_acc_max > 0 Then 'v_max wird erreicht
-            _t.t_ac = Math.Sqrt(2 * s_acc_max / _a_max)
-            _t.t_const = (_s - 2 * s_acc_max) / _v_max
-            _t.v_max_reached = True
+        If s - 2 * s_acc_max > 0 Then 'v_max wird erreicht
+            t.t_ac = t_ac
+            t.t_const = (s - 2 * s_acc_max) / v_max
+            t.v_max_reached = True
         Else 'v_max wird nicht erreicht
-            _t.t_ac = Math.Sqrt(_s / _a_max)
-            _t.t_const = 0
-            _t.v_max_reached = False
+            t.t_ac = (Math.Sqrt(2 * a_max * (s / 2) + v_min ^ 2) - v_min) / a_max
+            t.t_const = 0
+            t.v_max_reached = False
         End If
         'Benötigte Zeit für komplette Bewegung
-        Return 2 * _t.t_ac + _t.t_const
+        Return 2 * t.t_ac + t.t_const
     End Function
 End Class
